969582764d12cd59d63fbfcba1717d49f0203e24
Slavcho98 <101054039+Slavcho98@users.noreply.github.com>
2023-09-30 15:08:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+    navLinks.style.display = "block";
+    
+}
+
+function hideMenu() {
+    navLinks.style.display ="none";
+    
+}
